5b34df7391d75ed1fe8c3b8659bbed64b76954a0
Joey Dalrymple <xjoey96@gmail.com>
2022-02-16 16:02:39 -0700

    Finished dynamic menus

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let showHide = () => {
 }
 
 //Picture Carousel
-let pictureFrame = document.querySelector(".pictureHolder");
+let pictureFrame = document.querySelector(".pictureFrame");
 let left = document.createElement("button");
 left.textContent="<<";
 left.classList.add("left");
@@ -38,4 +38,30 @@ right.classList.add("right");
 pictureFrame.append(left, right)
 
 
+let pictureArray = ["picture1", "picture2", "picture3", "picture4", "picture5"];
+
+pictureFrame.classList.add(pictureArray[0]);
+let currentPic = 0;
+console.log(currentPic)
+
+right.addEventListener('click', () => {
+  pictureFrame.classList.remove(pictureArray[currentPic]);
+  if (currentPic === 4){
+    currentPic = 1;
+  } else {
+  currentPic += 1;
+  };
+  pictureFrame.classList.remove(pictureArray);
+  pictureFrame.classList.add(pictureArray[currentPic]);
+});
+left.addEventListener('click', () => {
+  pictureFrame.classList.remove(pictureArray[currentPic]);
+  if (currentPic === 0){
+    currentPic = 4;
+  } else {
+  currentPic -= 1;
+  }
+  pictureFrame.classList.remove(pictureArray[currentPic]);
+  pictureFrame.classList.add(pictureArray[currentPic]);
+});
 
